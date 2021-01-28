39b7b30c0df09b37b6cf4f609766b38dea1e0f3d
Danny Castillo <dannyhgo@hotmail.com>
2021-01-28 16:34:41 +0100

    Moving files to main branch

--- js/script.js
+++ js/script.js
@@ -1,49 +1,79 @@
 // Variables
-const left = document.querySelector('.left');
-const right= document.querySelector('.right');
-const center = document.querySelector('.center');
-const container = document.querySelector('.container');
-const leftVideo = document.querySelector('.video--deathmatch');
-const rightVideo = document.querySelector('.video--lambda');
-const centerVideo = document.querySelector('.video--pf2');
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const slidesLength = slideRight.querySelectorAll('div').length;
 
-document.onload = console.log("%c\"You might not think that programmers are artists, but programming is an extremely creative profession. It's logic-based creativity.\"%c-John Romero",
 
-    "color:#40e0d0;font-family:Helvetica;font-size:1rem;font-weight:bold", "font-family:system-ui;font-size:1rem;font-weight:bold"
 
-)
+let activeSlideIndex = 0;
 
-document.onload = console.log("if you have found this message then you should probabaly contact us for a new potential job");
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
-// Event Listeners Left
-left.addEventListener('mouseenter', () => {
-    container.classList.add('hover-left');
-    leftVideo.play();
+upButton.addEventListener('click', () => {
+    changeSlide('up');
+    changeColor('up');
 });
-left.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-left');
-    leftVideo.pause();
+downButton.addEventListener('click', () => {
+    changeSlide('down');
+    changeColor('down');
 });
 
-// Event Listener Right
-right.addEventListener('mouseenter', () => {
-    container.classList.add('hover-right');
-    rightVideo.play();
-});
-//right.addEventListener('mouseenter', () => 
-right.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-right');
-    rightVideo.pause();
-});
+// Slide Function
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight;
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0;
+        }
+    }
 
-// Event Listener Center
-center.addEventListener('mouseenter', () => {
-    container.classList.add('hover-center');
-    centerVideo.play();
-});
-center.addEventListener('mouseleave', () =>{
-    container.classList.remove('hover-center');
-    centerVideo.pause();
-});
+    if (direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1;
+        }
+    }
 
-    
+
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+};
+
+// Variables
+const colorArrayUp = ['#b73739', '#1f170b', '#121212', '#000'];
+let colorUp = 0;
+let colorDown = 2;
+
+// Change Color Function
+const changeColor = (direction) => {
+    if (direction === 'up') {
+        colorUp++;
+        colorDown++;
+        if (colorUp > slidesLength - 1) {
+            colorUp = 0;
+        }
+        if (colorDown > slidesLength - 1) {
+            colorDown = 0;
+        }
+
+    }
+
+    if (direction === 'down') {
+        colorUp--;
+        colorDown--;
+        if (colorUp < 0) {
+            colorUp = slidesLength - 1;
+        }
+        if (colorDown < 0) {
+            colorDown = slidesLength - 1;
+        }
+
+    }
+    upButton.style.background = `${colorArrayUp[colorUp]}`;
+    downButton.style.background = `${colorArrayUp[colorDown]}`;
+};
